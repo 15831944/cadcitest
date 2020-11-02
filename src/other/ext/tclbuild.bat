db67a4e77d480ae025ad62136bc8b9a980f8fcc6
Cliff Yapp <238416+starseeker@users.noreply.github.com>
2020-11-02 15:01:11 -0500

    Another try...

--- src/other/ext/tclbuild.bat
+++ src/other/ext/tclbuild.bat
@@ -1,4 +1,4 @@
 set VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise
 set DevEnvDir=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE
-call C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat
+start "" CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 nmake -f makefile.vc install INSTALLDIR=%1 SUFX=
