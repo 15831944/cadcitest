46c8854e16922607e2dceac902d23285a06f2a15
Cliff Yapp <238416+starseeker@users.noreply.github.com>
2020-11-02 15:51:15 -0500

    Trying to get a read on the environment...

--- src/other/ext/tcl.cmake
+++ src/other/ext/tcl.cmake
@@ -76,10 +76,10 @@ if (BRLCAD_TCL_BUILD)
     ExternalProject_Add(TCL_BLD
       URL "${CMAKE_CURRENT_SOURCE_DIR}/tcl"
       BUILD_ALWAYS ${EXTERNAL_BUILD_UPDATE} ${LOG_OPTS}
-      CONFIGURE_COMMAND ${VCVARS_BAT}
+      CONFIGURE_COMMAND SET
       BINARY_DIR ${TCL_SRC_DIR}/win
-      BUILD_COMMAND  nmake -f makefile.vc INSTALLDIR=${TCL_INSTDIR} SUFX=
-      INSTALL_COMMAND nmake -f makefile.vc install INSTALLDIR=${TCL_INSTDIR} SUFX=
+      BUILD_COMMAND ${VCVARS_BAT} && nmake -f makefile.vc INSTALLDIR=${TCL_INSTDIR} SUFX=
+      INSTALL_COMMAND ${VCVARS_BAT} && nmake -f makefile.vc install INSTALLDIR=${TCL_INSTDIR} SUFX=
       )
     set(TCL_APPINIT)
 
