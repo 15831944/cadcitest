cb43c3e1e2101577b6905b293e179cbcfdd0d315
Cliff Yapp <238416+starseeker@users.noreply.github.com>
2020-11-03 10:59:15 -0500

    implicit declarations

--- src/other/ext/stepcode/src/exppp/exppp.c
+++ src/other/ext/stepcode/src/exppp/exppp.c
@@ -5,6 +5,13 @@
 #include <errno.h>
 #include <stdarg.h>
 
+#ifdef HAVE_SYS_STAT_H
+# include <sys/stat.h>
+#endif
+#ifdef HAVE_UNISTD_H
+#  include <unistd.h>
+#endif
+
 #include "../express/expbasic.h"
 #include "../express/express.h"
 #include "exppp.h"
