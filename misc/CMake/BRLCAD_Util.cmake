a9527f93caa83e622c762a662daa785660758dc7
Cliff Yapp <238416+starseeker@users.noreply.github.com>
2020-12-04 08:58:14 -0500

    Latest extbuild

--- misc/CMake/BRLCAD_Util.cmake
+++ misc/CMake/BRLCAD_Util.cmake
@@ -38,6 +38,14 @@
 include(CMakeParseArguments)
 
 #-----------------------------------------------------------------------------
+# Find the executable extension, if there is one.  Really should be able to use
+# CMAKE_EXECUTABLE_SUFFIX for this, but we've hit a few cases over the years
+# where that hasn't been defined.  CMAKE_COMMAND does seem to be reliably
+# defined, however, so we establish the convention of using it to supply us
+# with the platform exe extension, if there is one.
+get_filename_component(EXE_EXT "${CMAKE_COMMAND}" EXT)
+
+#-----------------------------------------------------------------------------
 # We want to support a "distclean" build target that will clear all
 # CMake-generated files from a source directory in the case of an
 # in-source-dir configuration.  Not recommended, but we'll try to
@@ -566,6 +574,55 @@ int main(int argc, const char **argv) {
   endif(COMMAND distclean)
 
 endfunction(generate_dreport)
+
+# Wrap the platform specific naming conventions we must manage to copy
+# build output from 3rd party build systems.
+function(set_lib_vars RVAR root vmaj vmin vpatch)
+
+  # OpenBSD has its own naming conventions.  Set a platform variable based on
+  # the OS name so we can test for it succinctly.
+  if ("${CMAKE_SYSTEM}" MATCHES ".*OpenBSD.*")
+    set(OPENBSD ON)
+  endif ("${CMAKE_SYSTEM}" MATCHES ".*OpenBSD.*")
+
+  unset(${RVAR}_BASENAME)
+  unset(${RVAR}_STATICNAME)
+  unset(${RVAR}_SUFFIX)
+  unset(${RVAR}_SYMLINK1)
+  unset(${RVAR}_SYMLINK2)
+
+  if (MSVC)
+    set(${RVAR}_BASENAME ${root})
+    set(${RVAR}_STATICNAME ${root}-static)
+    set(${RVAR}_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_1 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_2 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX}.${vmaj})
+  elseif (APPLE)
+    set(${RVAR}_BASENAME lib${root})
+    set(${RVAR}_STATICNAME lib${root})
+    set(${RVAR}_SUFFIX .${vmaj}.${vmin}.${vpatch}${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_1 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_2 ${${RVAR}_BASENAME}.${vmaj}${CMAKE_SHARED_LIBRARY_SUFFIX})
+  elseif (OPENBSD)
+    set(${RVAR}_BASENAME lib${root})
+    set(${RVAR}_STATICNAME lib${root})
+    set(${RVAR}_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX}.${vmaj}.${vmin})
+  else (MSVC)
+    set(${RVAR}_BASENAME lib${root})
+    set(${RVAR}_STATICNAME lib${root})
+    set(${RVAR}_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX}.${vmaj}.${vmin}.${vpatch})
+    set(${RVAR}_SYMLINK_1 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${RVAR}_SYMLINK_2 ${${RVAR}_BASENAME}${CMAKE_SHARED_LIBRARY_SUFFIX}.${vmaj})
+  endif (MSVC)
+
+  # Communicate the answers to the parent scope - these are the return values.
+  set(${RVAR}_BASENAME   "${${RVAR}_BASENAME}"   PARENT_SCOPE)
+  set(${RVAR}_STATICNAME "${${RVAR}_STATICNAME}" PARENT_SCOPE)
+  set(${RVAR}_SUFFIX     "${${RVAR}_SUFFIX}"     PARENT_SCOPE)
+  set(${RVAR}_SYMLINK_1   "${${RVAR}_SYMLINK_1}"   PARENT_SCOPE)
+  set(${RVAR}_SYMLINK_2   "${${RVAR}_SYMLINK_2}"   PARENT_SCOPE)
+
+endfunction(set_lib_vars RVAR root)
 
 # Local Variables:
 # tab-width: 8
---
+++ src/other/ext/stepcode/include/express/memory.h
@@ -0,0 +1,6 @@
+#ifndef __MEMORY_H
+#define __MEMORY_H
+
+void MEMORYinitialize();
+
+#endif // __MEMORY_H
